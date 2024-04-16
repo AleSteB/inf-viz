3a755f09f4676cc9477eb0da069f956cfcc82eb7
AlexB <55703624+AleSteB@users.noreply.github.com>
2024-04-16 09:48:37 +0200

    up

--- Grapher.py
+++ Grapher.py
@@ -66,13 +66,13 @@ def md_from_onto_obj(indv, ontology_name):
     mermaid_str = '```mermaid\n graph TD;\n'
     
     mermaid_str = triple_to_mermaid(indv, mermaid_str)
-    
+    """
     for prop in list(indv.get_properties()):
         if type(prop) == owlready2.prop.ObjectPropertyClass:
             pred_list = eval("indv.{}".format(prop.name))
             for obj in pred_list:
                 mermaid_str = triple_to_mermaid(obj, mermaid_str)
-    
+    """
     mermaid_str +='```'
     
     with open(ontology_name.replace(".owl",".md"),"w") as file:
@@ -83,23 +83,21 @@ def md_from_onto_obj(indv, ontology_name):
 ontology_name = "reac4cat_with_examples.owl"
 searched_name = "Reaction_1"
 
-
 onto1 = owlready2.World()
 onto1 = get_ontology(ontology_name).load()
+indv1 = onto1.search_one(label = searched_name)
+md_from_onto_obj(indv1,ontology_name)
 
-#onto2 = owlready2.World()
-#onto2 = get_ontology(ontology_name.replace(".owl","_inferred.owl")).load()
+
+onto2 = owlready2.World()
+onto2 = get_ontology(ontology_name.replace(".owl","_inferred.owl")).load()
 
 #with onto2:
 #    sync_reasoner(infer_property_values = True)
 #    onto2.save(ontology_name.replace(".owl","_inferred.owl"))
-
-#onto2 = get_ontology("reac4cat_with_examples_inferred2.owl").load()
-
-indv1 = onto1.search_one(label = searched_name)
+#
+#onto2 = get_ontology("reac4cat_with_examples_inferred.owl").load()
 indv2 = onto2.search_one(label = searched_name)
-
-md_from_onto_obj(indv1,ontology_name)
 md_from_onto_obj(indv2,ontology_name.replace(".owl","_inferred.owl"))
 
 
